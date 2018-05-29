15d4a44c3dd3726bf061f59c83f394af5b5d3e68
Lsong <song940@gmail.com>
2018-05-29 13:24:47 +0800

    modify falcon

--- index.js
+++ index.js
@@ -1,58 +1,59 @@
-const os 			= require('os');
-const request = require('superagent');
-const debug   = require('debug')('open-falcon');
+const os = require('os');
+const URI = require('url');
+const http = require('http');
 
-function Falcon(options){
-  if(!(this instanceof Falcon)){
+function Falcon(options) {
+  if (!(this instanceof Falcon)) {
     return new Falcon(options);
   }
-  if(typeof options == 'string'){
+  if (typeof options == 'string') {
     options = {
       api: options
     };
   }
-  var self = this, defaults = {
-    tags        : ''           ,
-    step        : 60           ,
-    value       : 0            ,
-    counterType : 'GAUGE'      ,
-    endpoint    : os.hostname(),
-    api         : 'http://127.0.0.1:1988/v1/push',
-    timestamp   : function(){
-      return Math.floor(+new Date() / 1000);
-    },
-  };
+  var self = this,
+    defaults = {
+      tags: '',
+      step: 60,
+      value: 0,
+      counterType: 'GAUGE',
+      endpoint: os.hostname(),
+      api: 'http://127.0.0.1:1988/v1/push',
+      timestamp: function () {
+        return Math.floor(+new Date() / 1000);
+      },
+    };
   //
-  for(var key in options){
-    defaults[ key ] = options[ key ];
+  for (var key in options) {
+    defaults[key] = options[key];
   }
   //
-  this.data    = {};
-  this.queue   = [];
+  this.data = {};
+  this.queue = [];
   this.options = defaults;
   return this;
 }
 
 Falcon.COUNTER_TYPE = {
-  GAUGE  : 'GAUGE',
+  GAUGE: 'GAUGE',
   COUNTER: 'COUNTER'
 };
 
-Falcon.prototype.use = function(middleware){
-  middleware.apply(this, [ this ]);
+Falcon.prototype.use = function (middleware) {
+  middleware.apply(this, [this]);
   return this;
 };
 
-Falcon.prototype.set = function(key, value){
-  this.data[ key ] = value;
+Falcon.prototype.set = function (key, value) {
+  this.data[key] = value;
   return this;
 };
 
-Falcon.prototype.metric = function(name, value){
-  if(name){
+Falcon.prototype.metric = function (name, value) {
+  if (name) {
     this.set('metric', name);
   }
-  if(value){
+  if (value) {
     this.set('value', value);
   }
   return this;
@@ -61,76 +62,76 @@ Falcon.prototype.metric = function(name, value){
  * [function description]
  * @return {[type]} [description]
  */
-Falcon.prototype.endpoint = function(value){
-  this.set.apply(this, [ 'endpoint', value ]);
+Falcon.prototype.endpoint = function (value) {
+  this.set.apply(this, ['endpoint', value]);
   return this;
 };
 /**
  * [function description]
  * @return {[type]} [description]
  */
-Falcon.prototype.timestamp = function(value){
-  this.set.apply(this, [ 'timestamp', value ]);
+Falcon.prototype.timestamp = function (value) {
+  this.set.apply(this, ['timestamp', value]);
   return this;
 };
 /**
  * [function description]
  * @return {[type]} [description]
  */
-Falcon.prototype.value = function(value){
-  this.set.apply(this, [ 'value', value ]);
+Falcon.prototype.value = function (value) {
+  this.set.apply(this, ['value', value]);
   return this;
 };
 /**
  * [function description]
  * @return {[type]} [description]
  */
-Falcon.prototype.step = function(value){
-  this.set.apply(this, [ 'step', value ]);
+Falcon.prototype.step = function (value) {
+  this.set.apply(this, ['step', value]);
   return this;
 };
 /**
  * [function description]
  * @return {[type]} [description]
  */
-Falcon.prototype.type = function(value){
-  this.set.apply(this, [ 'counterType', value.toUpperCase() ]);
+Falcon.prototype.type = function (value) {
+  this.set.apply(this, ['counterType', value.toUpperCase()]);
   return this;
 };
 /**
  * [function description]
  * @return {[type]} [description]
  */
-Falcon.prototype.tags = function(tags){
+Falcon.prototype.tags = function (tags) {
   var arr = [];
-  Object.keys(tags || {}).map(function(key){
-    arr.push([ key, tags[key] ].join('='));
+  Object.keys(tags || {}).map(function (key) {
+    arr.push([key, tags[key]].join('='));
   });
-  this.data[ 'tags' ] = arr.join(',');
+  this.data['tags'] = arr.join(',');
   return this;
 };
 
-Falcon.prototype.end = function(){
+Falcon.prototype.end = function () {
   var self = this;
   ([
-    'step'        ,
-    'tags'        ,
-    'value'       ,
-    'metric'      ,
-    'endpoint'    ,
-    'timestamp'   ,
+    'step',
+    'tags',
+    'value',
+    'metric',
+    'endpoint',
+    'timestamp',
     'counterType'
-  ]).map(function(key){
-    if(
-      typeof self.data[ key ]  == 'undefined' &&
+  ]).map(function (key) {
+    if (
+      typeof self.data[key] == 'undefined' &&
       typeof self.options[key] == 'undefined'
-    ) throw new Error( `'${key}' is required .`);
+    ) throw new Error(`'${key}' is required .`);
     //
-    if(typeof self.data[key] == 'undefined'){
-      if(typeof self.options[key] == 'function'){
-        self.data[ key ] = self.options[key](self);
-      }else if(typeof self.options[key] != 'undefined'){
-        self.data[ key ] = self.options[key];
+    if (typeof self.data[key] == 'undefined') {
+      if (typeof self.options[key] == 'function') {
+        self.data[key] = self.options[key](self);
+      } else if (typeof self.options[key] != 'undefined') {
+        self.data[key] = self.options[key];
       }
     }
   });
@@ -143,15 +144,22 @@ Falcon.prototype.end = function(){
  * [function description]
  * @return {[type]} [description]
  */
-Falcon.prototype.send = function(callback){
-  var self = this;
-  request
-    .post(this.options.api)
-    .send(this.queue)
-    .end(function(){
-      callback && callback.apply(self, arguments);
-      self.queue = [];
-    });
+Falcon.prototype.send = function (callback = () => {}) {
+  const { api } = this.options;
+  const req = http.request(Object.assign(URI.parse(api), {
+    method: 'POST'
+  }), res => {
+    const buffer = [];
+    res
+      .on('error', callback)
+      .on('data', chunk => buffer.push(chunk))
+      .on('end', () => {
+        callback && callback(null, Buffer.concat(buffer));
+        this.queue = [];
+      });
+  })
+  req.on('error', callback);
+  req.end(JSON.stringify(this.queue));
   return this;
 };
 
